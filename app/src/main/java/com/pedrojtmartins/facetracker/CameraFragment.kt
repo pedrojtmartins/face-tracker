b051ec2269614274123e6640cdef3a9b558cbec4
Pedro Martins <pedro.martins@yoti.com>
2021-02-03 00:24:44 +0000

    Add camera fragment with permissions

---
+++ app/src/main/java/com/pedrojtmartins/facetracker/CameraFragment.kt
@@ -0,0 +1,95 @@
+package com.pedrojtmartins.facetracker
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.pedrojtmartins.facetracker.databinding.FragmentCameraBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_camera.*
+
+private const val TAG = "CameraFragment"
+private const val REQUEST_CODE_PERMISSIONS = 1
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+class CameraFragment : Fragment() {
+
+    private var _binding: FragmentCameraBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            showPermissionsRequest()
+        }
+    }
+
+    private fun startCamera(cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA) {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                    .build()
+                    .also { it.setSurfaceProvider(binding.previewView.surfaceProvider) }
+
+            try {
+                with(cameraProvider) {
+                    unbindAll()
+                    bindToLifecycle(this@CameraFragment, cameraSelector, preview)
+                }
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            startCamera()
+        } else {
+            // TODO Improve permissions handling
+            requireActivity().finish()
+        }
+    }
+
+    private fun showPermissionsRequest() {
+        // TODO Improve permissions handling
+        requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+}
